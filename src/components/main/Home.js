f90cd4ca9194c9db4793379dc71b0529b1150dd0
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-04 12:27:08 -0700

    Body Updated

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,6 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import RegImage from '../images/registration (2).png'
 import RegProcess from '../images/registration.png'
+import Tick from '../images/status.png'
+import VaccineCard from '../images/vaccine_card.png'
+import Certificate from '../images/certificate.png'
+import Faq from '../images/faq.png'
 function Home() {
   return (
     <div>
@@ -33,7 +38,34 @@ function Home() {
                 <div className="col-lg-6">
                     {/* Check status */}
                     <div className="home-checkstatus">
-                        <h3>I am from Check Status</h3>
+                        <div className="home-checkstatus1">
+                                <div className="checkstatus-img-text">
+                                     {/* Image and Text.. */}
+                                     <img src={Tick} alt="" srcset="" />
+                                     <a href="#">CHECK STATUS</a>
+                                </div>
+                        </div>
+                        <div className="home-checkstatus1 vaccineCard">
+                                <div className="checkstatus-img-text">
+                                     {/* Image and Text.. */}
+                                     <img src={VaccineCard} alt="" srcset="" />
+                                     <a href="#">VACCINE CARD</a>
+                                </div>
+                        </div>
+                        <div className="home-checkstatus1 certificate">
+                                <div className="checkstatus-img-text">
+                                     {/* Image and Text.. */}
+                                     <img src={Certificate} alt="" srcset="" />
+                                     <a href="#">CERTIFICATE</a>
+                                </div>
+                        </div>
+                        <div className="home-checkstatus1 faq">
+                                <div className="checkstatus-img-text">
+                                     {/* Image and Text.. */}
+                                     <img src={Faq} alt="" srcset="" />
+                                     <a href="#">FAQ</a>
+                                </div>
+                        </div>
                     </div>
 
                 </div>
