eb6a4c90dba5b96c1187a7edc441b821d910a666
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-04 02:29:57 -0700

    Updated.....

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from './Header'
 
 function Body() {
   return (
     <div className='body'>
 
- 
     </div>
   )
 }
--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import RegImage from '../images/registration (2).png'
+import RegProcess from '../images/registration.png'
 function Home() {
   return (
     <div>
@@ -7,10 +9,32 @@ function Home() {
             <div className="row">
                 <div className="col-lg-6">
                         {/* Vaccine Registration  */}
+                        <div className="home-vaccinereg">
+                            <div className="vaccinereg-box">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                         <img src={RegImage} alt="" srcset="" />
+                                    </div>
+                                    <div className="col-lg-6 vc-reg">
+                                            <h4>VACCINE REGISTRATION</h4>
+                                            
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-lg-12 vc-reg1">
+                                            <h6>During Registration You Need</h6>
+                                            <img src={RegProcess} className="RegProcess" alt="" srcset="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         
                 </div>
                 <div className="col-lg-6">
                     {/* Check status */}
+                    <div className="home-checkstatus">
+                        <h3>I am from Check Status</h3>
+                    </div>
 
                 </div>
             </div>
