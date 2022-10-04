cccd9894936d1e9c0091ebb1b0e6ad8635646380
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-04 01:03:56 -0700

    Update Background

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,16 +1,17 @@
 import React from 'react'
-
 function Home() {
   return (
-    <div className='body'>
+    <div>
     
         <div className="home-body">
             <div className="row">
                 <div className="col-lg-6">
                         {/* Vaccine Registration  */}
+                        
                 </div>
                 <div className="col-lg-6">
                     {/* Check status */}
+
                 </div>
             </div>
         </div>
