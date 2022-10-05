7b37ffc1dc3d7ca69d47d538d3e553e863d47c1f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-04 23:33:33 -0700

    Complete HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
        
-       <Header/>     
+       <Header/> 
+
+
+       <Footer/>    
    </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,38 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-
+import '../App.css'
+import FooterIcon from './images/surokkhaHeader.png'
+import Ict from './images/ict.png'
+import Affiliate from './images/footerBanner.png'
 function Footer() {
   return (
     <div className='footer'>
-        
+        <div className="row">
+          <div className="col-lg-4">
+                <div className="row">
+                  <div className="col-lg-6">
+                      <img src={FooterIcon} className="footerIcon" alt="Surokkha Apps" />
+                  </div>
+                  <div className="col-lg-6">
+                      <div className="footer-list">
+                        <a href="#"><h6>FAQ</h6></a>
+                        <a href="#"><h6>Manual</h6></a>
+                        <a href="#"><h6>Privacy Policy</h6></a>
+                        <a href="#"><h6>Terms of Service</h6></a>
+                        <a href="#"><h6>Other Affiliate</h6></a>
+                      </div>
+                  </div>
+                </div>
+          </div>
+          <div className="col-lg-4">
+              <h6 className='developby'>Developed by- Sm Munna <span><i>For Practicing</i></span></h6>
+              <img src={Ict} alt="ICT" />
+          </div>
+          <div className="col-lg-4">
+              <h6 className='developby'>Affiliates</h6>
+              <img src={Affiliate} alt="Affiliate" />
+          </div>
+        </div>
     </div>
   )
 }
