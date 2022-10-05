1492c5a9cae2bd712696869f6e2a2d6610a35ce4
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-05 15:51:55 -0700

    Updated

--- src/components/main/Registration.js
+++ src/components/main/Registration.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../../App.css'
+import '../css/registration.css'
 function Registration() {
   return (
     <div className='body'>
          <h2> Registration</h2>
+         <div className="mydiv">
+            <div className="inner1">
+
+            </div>
+         </div>
     </div>
   )
 }
